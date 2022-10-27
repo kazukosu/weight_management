1dd960f21a33472fcda874f142be7410fc6c77e2
Ryo412 <ryo.matsuura.412@gmail.com>
2022-10-27 15:01:25 +0900

    add weight table

--- db.py
+++ db.py
@@ -16,8 +16,22 @@ cur.execute(
     sex CHAR(1),
     age INT(1),
     height INT(1),
-    PRIMARY KEY (id))
+    PRIMARY KEY (id));
+
+    CREATE TABLE mw.weight
+    (weight_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    user_id MEDIUMINT NOT NULL,
+    date datetime,
+    weight INT(1),
+    PRIMARY KEY (weight_id),
+    FOREIGN KEY (user_id) REFERENCES user(id))
     """
 )
+
+"""
+
+
+
+"""
 con.commit()
 con.close()
